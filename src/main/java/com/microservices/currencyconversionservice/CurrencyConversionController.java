9180f6ebf20d3a0c99c87493c239f4e532c77b40
SylwiaWojda <sylwiaaa001@wp.pl>
2022-09-27 11:36:54 +1000

    Service for currency conversion

---
+++ src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,22 @@
+package com.microservices.currencyconversionservice;
+
+import org.apache.catalina.startup.Bootstrap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+            ) {
+    return  new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+
+}
